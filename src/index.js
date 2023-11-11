fc9c74f13b870eb5ac608995fed05f4da15ca982
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-11-11 14:46:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
-import { fetchAllPictures } from './api';
-import {q} from './api'
+// import { fetchAllPictures } from './api';
 
 const form = document.querySelector('.search-form');
 const input = document.querySelector('input');
 const btn = document.querySelector("button");
+
+//ось це вже є у файлі api.jsю його можна імопртувати
+const key = "40611868-10084cd142e7b08f59941726f";
+let q = "cat";
+const imageType = 'photo';
+const imageOrientation = 'horizontal';
+const safesearch = true;
+export function fetchAllPictures(){
+return fetch(`https://pixabay.com/api/?key=${key}&q=${q}&image_type=${imageType}&orientation=${imageOrientation}&safesearch=${safesearch}`)
+.then(response => response.json())
+}
 
 //додаємо слухача на форму
 form.addEventListener("submit", onFormSubmit) ;
