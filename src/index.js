4a338c530461595075825d7815be98c8e3bd5dde
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-11-11 15:24:22 +0200

    фва

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import axios from 'axios';
 const form = document.querySelector('.search-form');
 const input = document.querySelector('input');
 const btn = document.querySelector("button");
+const container = document.querySelector('.gallery')
 
-//ось це вже є у файлі api.jsю його можна імопртувати
+//змінні для запиту на сервер
 const key = "40611868-10084cd142e7b08f59941726f";
 let q = "cat";
 const imageType = 'photo';
 const imageOrientation = 'horizontal';
 const safesearch = true;
+
 export function fetchAllPictures(){
 return fetch(`https://pixabay.com/api/?key=${key}&q=${q}&image_type=${imageType}&orientation=${imageOrientation}&safesearch=${safesearch}`)
 .then(response => response.json())
@@ -23,12 +25,43 @@ function onFormSubmit(evt) {
 
     q = input.value;
 
-// /запит на pixabay.com. Вивівся масив об'єктів
+//запит на pixabay.com. Вивівся масив об'єктів
 fetchAllPictures()
 .then(data => {
-    console.log(data);
-    return data;
-}).catch(error => {
+    if (data.hits && data.hits.length > 0){
+        console.log(data);
+        // return data;
+        renderCards(data.hits);
+    } else {
+        console.log('Sorry, there are no images matching your search query. Please try again.');
+    }
+   }).catch(error => {
     console.log("error");
 })
+}
+
+function renderCards(hits) {
+    container.innerHTML = '';
+
+    const cardMarkup = hits.map(card =>`
+    <div class="photo-card">
+  <img src="${card.pageURL}" alt="${card.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>${card.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>${card.views}</b>
+    </p>
+    <p class="info-item">
+      <b>${card.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>${card.downloads}s</b>
+    </p>
+  </div>
+</div>
+    `).join('');
+
+    container.insertAdjacentHTML("beforeend", cardMarkup);
 }
