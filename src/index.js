0eb195a08f93e09975a5fbf9664dc9f6ba0d2498
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-11-12 08:59:24 +0200

    початок роботи зі стилями

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 const form = document.querySelector('.search-form');
 const input = document.querySelector('input');
-const searchBtn = document.querySelector("button");
+const searchBtn = document.querySelector('button');
 const container = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
 loadBtn.setAttribute('disabled', 'true');
+input.classList.add('input-style');
+searchBtn.classList.add('searchBt-style');
 
 //змінні для запиту на сервер
-const key = "40611868-10084cd142e7b08f59941726f";
-let q = "cat";
+const key = '40611868-10084cd142e7b08f59941726f';
+let q = 'cat';
 const imageType = 'photo';
 const imageOrientation = 'horizontal';
 const safesearch = true;
@@ -20,43 +22,49 @@ let page = 1;
 const amountPerPage = 10;
 let totalPage = 1;
 
-function fetchAllPictures(){
-  // page = 1;
-return fetch(`https://pixabay.com/api/?key=${key}&q=${q}&image_type=${imageType}&orientation=${imageOrientation}&safesearch=${safesearch}&page=${page}&per_page=${amountPerPage}`)
-.then(response => response.json())
+function fetchAllPictures() {
+ 
+  return fetch(
+    `https://pixabay.com/api/?key=${key}&q=${q}&image_type=${imageType}&orientation=${imageOrientation}&safesearch=${safesearch}&page=${page}&per_page=${amountPerPage}`
+  ).then(response => response.json());
 }
 
 //додаємо слухача на форму
-form.addEventListener("submit", onFormSubmit) ;
+form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    q = input.value;
-
-//запит на pixabay.com. Вивівся масив об'єктів
-fetchAllPictures()
-.then(data => {
-    if (data.hits && data.hits.length > 0){
+  q = input.value;
+  page = 1;
+  //запит на pixabay.com. Вивівся масив об'єктів
+  fetchAllPictures()
+    .then(data => {
+      if (data.hits && data.hits.length > 0) {
         console.log(data);
         renderCards(data.hits);
-    } else {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.',  {
-    timeout: 6000,
-  },
-        function cb() { });       
-    }
-    totalPage = Math.ceil(data.total / amountPerPage);
-    checkLoadBtnStatus();
-   }).catch(error => {
-    console.log("error");
-})
+      } else {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+          {
+            timeout: 6000,
+          },
+          function cb() {}
+        );
+      }
+      totalPage = Math.ceil(data.total / amountPerPage);
+      checkLoadBtnStatus();
+    })
+    .catch(error => {
+      console.log('error');
+    });
 }
 
 function renderCards(hits) {
-    container.innerHTML = '';
+  container.innerHTML = '';
 
-    const cardMarkup = hits.map(card =>`
+  const cardMarkup = hits
+    .map(
+      card => `
     <div class="photo-card">
   <img src="${card.previewURL}" alt="${card.tags}" loading="lazy" />
   <div class="info">
@@ -74,35 +82,40 @@ function renderCards(hits) {
     </p>
   </div>
 </div>
-    `).join('');
+    `
+    )
+    .join('');
 
-    container.insertAdjacentHTML("beforeend", cardMarkup);
-    loadBtn.removeAttribute('disabled', 'true');
-};
+  container.insertAdjacentHTML('beforeend', cardMarkup);
+  loadBtn.removeAttribute('disabled', 'true');
+}
 //завантаження по load more
 loadBtn.addEventListener('click', onLoadMoreClick);
-function onLoadMoreClick(evt){
-page += 1;
+function onLoadMoreClick(evt) {
+  page += 1;
 
   fetchAllPictures()
-  .then(data => {
-      if (data.hits && data.hits.length > 0){
-          console.log(data);
-          renderCards(data.hits);
+    .then(data => {
+      if (data.hits && data.hits.length > 0) {
+        console.log(data);
+        renderCards(data.hits);
       } else {
         Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.',  {
-      timeout: 6000,
-    },
-          function cb() { });       
+          'Sorry, there are no images matching your search query. Please try again.',
+          {
+            timeout: 6000,
+          },
+          function cb() {}
+        );
       }
-     }).catch(error => {
-      console.log("error");
-  });
-  checkLoadBtnStatus()
+    })
+    .catch(error => {
+      console.log('error');
+    });
+  checkLoadBtnStatus();
 }
-function checkLoadBtnStatus(){
- if (page >= totalPage) {
-  loadBtn.setAttribute('disabled', 'true');
- }
+function checkLoadBtnStatus() {
+  if (page >= totalPage) {
+    loadBtn.setAttribute('disabled', 'true');
+  }
 }
